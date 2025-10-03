588aca2059440f17c8fcaeea4eee28ff36495a21
Yuri <yuripiresalves@gmail.com>
2025-10-03 15:18:31 -0300

    refactor: update NotesSection to remove commented code and add 'Soon' status indicator

--- src/components/notes-section.tsx
+++ src/components/notes-section.tsx
@@ -4,8 +4,7 @@ export function NotesSection() {
   return (
     <section className="flex flex-col gap-4 border border-paragraph/10 rounded-lg overflow-hidden px-4 py-7">
       <h2 className="text-xl font-bold px-3">Notes</h2>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-4 md:gap-y-2">
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-4 md:gap-y-2">
         <Link
           href="#"
           className="px-3 py-3 rounded-lg border md:border-transparent md:bg-transparent border-paragraph/7 bg-foreground hover:border-paragraph/7 hover:bg-foreground transition-colors duration-500"
@@ -88,7 +87,10 @@ export function NotesSection() {
         className="mt-1 flex items-center justify-center p-2 rounded-lg border border-paragraph/7 md:bg-foreground font-semibold text-paragraph hover:text-paragraph/70 hover:border-paragraph/7 transition-colors duration-300"
       >
         View all
-      </Link>
+      </Link> */}
+      <div className="mt-1 flex items-center justify-center p-2 rounded-lg border border-paragraph/7 md:bg-foreground font-semibold text-paragraph hover:text-paragraph/70 hover:border-paragraph/7 transition-colors duration-300">
+        <span className="text-sm text-paragraph/70">Soon</span>
+      </div>
     </section>
   );
 }
