9ce435b0c6b7e48fe59ff6fa65b5d1f7a016e7f3
Yuri <yuripiresalves@gmail.com>
2025-10-03 17:35:19 -0300

    feat: add Microsoft Clarity script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Clarity from "@microsoft/clarity";
 import type { Metadata } from "next";
 import { Ubuntu_Sans } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const ubuntuSans = Ubuntu_Sans({
   variable: "--font-ubuntu-sans",
@@ -33,6 +34,15 @@ export default function RootLayout({
         {children}
         <Analytics />
         <SpeedInsights />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "tkk6cu6lj7");
+            `}
+        </Script>
       </body>
     </html>
   );
