f86db3f05d52961e6f96141a8cbaa801aac921f8
Yuri <yuripiresalves@gmail.com>
2025-10-03 17:02:50 -0300

    remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,19 +4,12 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Ubuntu_Sans } from "next/font/google";
 import "./globals.css";
-import { ThemeSwitcher } from "../../theme";
-import Link from "next/link";
 
 const ubuntuSans = Ubuntu_Sans({
   variable: "--font-ubuntu-sans",
   subsets: ["latin"],
   weight: ["400", "600", "700"],
 });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "YURiPA",
