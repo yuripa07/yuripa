de2822bc9d94c5ddf4a479e9ac14170681e7a29b
Yuri <yuripiresalves@gmail.com>
2025-10-06 08:35:19 -0300

    add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Clarity from "@microsoft/clarity";
 
 import type { Metadata } from "next";
 import { Ubuntu_Sans } from "next/font/google";
@@ -23,26 +22,34 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  if (typeof window !== "undefined") {
-    const projectId = "tkk6cu6lj7";
-    Clarity.init(projectId);
-  }
-
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KL8CN8G2');
+          `}
+        </Script>
+      </head>
+
       <body className={`${ubuntuSans.variable} min-h-screen antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KL8CN8G2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
+
         <Analytics />
         <SpeedInsights />
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`
-              (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "tkk6cu6lj7");
-            `}
-        </Script>
       </body>
     </html>
   );
